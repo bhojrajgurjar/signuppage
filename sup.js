780f1e42d93afae92a2fb8e0e8fc39f2e4188467
DELL LPPY <bgurjar2000@gmail.com>
2021-01-22 10:31:19 +0530

    lastcommit

--- sup.js
+++ sup.js
@@ -5,6 +5,7 @@ function afterClick(){
     var y =document.getElementById('usrname')
     var z =document.getElementById('pssword')
     var zz =document.getElementById('confirmPasswaor')
+    
    
     if(x.value == null || x.value ==""){
         alert("ENTER  EMAIL ID")
@@ -15,19 +16,32 @@ function afterClick(){
     else if(z.value == null || z.value ==""){
         alert("ENTER PASSWORD")
     }
+    else if(z.value.length<4){
+        alert( " you have to enter at least 4 digit password!")
+        z.value = null
+        zz.value = null
+    }
     else if( zz.value !==z.value){
         alert("THE PASSWORDS DON'T MATCH")
+        zz.value = null
     }
     else{
         alert("SIGN UP SUCCESSFULLY")
-        x.value =''
-        y.value=''
-        z.value=''
-        zz.value=''
+        x.value =null
+        y.value=null
+        z.value=null
+        zz.value=null
     }
     
     
-}   
+}
+
+var password = document.getElementById('pssword').addEventListener('mouseenter',validation)
+
+function validation(e){
+    var temp = document.getElementById('temp')
+    temp.style.display ='inline';
+}
 
 var reset = document.getElementById('reset').addEventListener('click',clear)
 
